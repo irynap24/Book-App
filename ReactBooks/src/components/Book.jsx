cd9e875dce91b7801787dd5218956a61f7886844
Iryna Pih <irynapih@gmail.com>
2024-10-01 23:01:14 -0400

    truncated description, added read more/less btn

--- ReactBooks/src/components/Book.jsx
+++ ReactBooks/src/components/Book.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Book = ({ book }) => {
   const title = book.volumeInfo.title;
   const authors = book.volumeInfo.authors ? book.volumeInfo.authors.join(', ') : 'Unknown Author';
+  const description = book.volumeInfo.description || 'No description available.';
   const thumbnail = book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : '';
+
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const maxLength = 150; // Maximum length of description before truncation
+
+  const toggleDescription = () => {
+    setIsExpanded(!isExpanded);
+  };
 
   return (
     <div className="book">
-      <h4>{title}</h4>
-      <p>Author(s): {authors}</p>
+      <h2>{title}</h2>
+      <p><strong>Author(s):</strong> {authors}</p>
       {thumbnail && <img src={thumbnail} alt={title} />}
+      <div className="description-container">
+        <p>
+          <strong>Description:</strong> {isExpanded ? description : `${description.substring(0, maxLength)}...`}
+        </p>
+        {description.length > maxLength && (
+          <button onClick={toggleDescription} className="toggle-button">
+            {isExpanded ? 'Show Less' : 'Read More'}
+          </button>
+        )}
+      </div>
     </div>
   );
 };
